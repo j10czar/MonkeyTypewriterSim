99a4d0f760e05411f8634d7daea2312067d09607
j10czar <jasontenczar@gmail.com>
2022-04-23 13:38:42 -0400

    test 3

--- MonkeyTypewriterSimulation/src/tester.java
+++ MonkeyTypewriterSimulation/src/tester.java
@@ -9,6 +9,9 @@ public class tester extends Thread{
 	private String target;
 	private int numMonkeys;
 	private static GUI gui;
+	private String testing;
+	private String testingsa;
+	
 
 	
 	public tester(String t , int n)
